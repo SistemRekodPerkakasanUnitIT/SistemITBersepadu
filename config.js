1211010b45aea35f20fac43a90f0aa0bb4d8b48d
SistemRekodPerkakasanUnitIT <pkpj2026@gmail.com>
2026-08-16 22:52:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ window.PORTAL_CONFIG = {
   API_URL: "https://script.google.com/macros/s/AKfycbxaSsEel0S3XQkIm5rmNwtGlsVHUpHfjIfYQisCkepXdJTcjGj9J6_S4x-aKG7uKcPH/exec",
 
   // Letak fail login.mp4 di root GitHub, atau tukar kepada URL MP4 terus.
-  LOGIN_VIDEO_URL: "login.mp4",
+  LOGIN_VIDEO_URL: "./login.mp4",
   ENABLE_LOGIN_VIDEO: true
 };
