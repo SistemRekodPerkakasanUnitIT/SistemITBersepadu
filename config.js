4c3c57658303f8355d2cf2ad60e7b391770771bc
SistemRekodPerkakasanUnitIT <pkpj2026@gmail.com>
2026-08-16 21:31:35 +0800

    Add portal configuration with API URL

---
+++ config.js
@@ -0,0 +1,4 @@
+// Tukar URL di bawah selepas Google Apps Script dideploy sebagai Web App.
+window.PORTAL_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbxaSsEel0S3XQkIm5rmNwtGlsVHUpHfjIfYQisCkepXdJTcjGj9J6_S4x-aKG7uKcPH/exec"
+};
