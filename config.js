37cf6051015834fff5f9def75b62a8624707a7b1
SistemRekodPerkakasanUnitIT <pkpj2026@gmail.com>
2026-08-16 22:09:58 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
-// Tukar URL di bawah selepas Google Apps Script dideploy sebagai Web App.
 window.PORTAL_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxaSsEel0S3XQkIm5rmNwtGlsVHUpHfjIfYQisCkepXdJTcjGj9J6_S4x-aKG7uKcPH/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbxaSsEel0S3XQkIm5rmNwtGlsVHUpHfjIfYQisCkepXdJTcjGj9J6_S4x-aKG7uKcPH/exec",
+
+  // Letak fail login.mp4 di root GitHub, atau tukar kepada URL MP4 terus.
+  LOGIN_VIDEO_URL: "login.mp4",
+  ENABLE_LOGIN_VIDEO: true
 };
